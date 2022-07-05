865caf7a34e6814ebfcf41a17bbfb5352b1fa75d
utto2000 <bartek200039@gmail.com>
2022-07-05 17:48:28 +0200

    Add register to HomeController

--- src/main/java/com/wator/organizer/controllers/HomeController.java
+++ src/main/java/com/wator/organizer/controllers/HomeController.java
@@ -7,10 +7,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
-    @RequestMapping("/")
-    @ResponseBody
-    public String index(){
-        return "Hello World";
+    @RequestMapping("/register")
+    public String register(){
+        return "register";
     }
 
 
