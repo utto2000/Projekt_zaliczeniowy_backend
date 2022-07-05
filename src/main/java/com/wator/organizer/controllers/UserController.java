e9f5f1151335ae5d2a9caed47594bf4ee6551143
utto2000 <bartek200039@gmail.com>
2022-07-05 20:48:33 +0200

    Fix user controller

--- src/main/java/com/wator/organizer/controllers/UserController.java
+++ src/main/java/com/wator/organizer/controllers/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UserController {
@@ -14,9 +15,13 @@ public class UserController {
             value = "/user/add",
             method = RequestMethod.POST,
             consumes = MediaType.APPLICATION_JSON_VALUE
+
     )
+    @ResponseBody
     public Object addUser(@RequestBody UserEntity data){
         return data;
     }
+
+
 
 }
