d8ec567f2ec0a613ce4399275083ddbe398fc96a
utto2000 <bartek200039@gmail.com>
2022-07-05 15:56:01 +0200

    Create project

---
+++ src/main/java/com/wator/organizer/OrganizerApplication.java
@@ -0,0 +1,13 @@
+package com.wator.organizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganizerApplication.class, args);
+	}
+
+}
