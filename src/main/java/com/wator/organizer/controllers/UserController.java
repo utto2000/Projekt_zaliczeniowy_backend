af3f87b2d04851910c855ff88d90005eda1f45cc
utto2000 <bartek200039@gmail.com>
2022-07-06 12:47:30 +0200

    Add register user to database

--- src/main/java/com/wator/organizer/controllers/UserController.java
+++ src/main/java/com/wator/organizer/controllers/UserController.java
@@ -1,7 +1,10 @@
 package com.wator.organizer.controllers;
 
 import com.wator.organizer.UserEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.http.MediaType;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +12,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class UserController {
-
+public class UserController implements CommandLineRunner {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
     @RequestMapping(
             value = "/user/add",
             method = RequestMethod.POST,
@@ -19,9 +23,18 @@ public class UserController {
     )
     @ResponseBody
     public Object addUser(@RequestBody UserEntity data){
+        String sql = "INSERT INTO users (FirstName, SecondName, Email, UserPassword) VALUES (?, ?, ?, ? )";
+        int result = jdbcTemplate.update(sql, data.getFirstName(), data.getSecondName(), data.getEmail(), data.getPassword());
+        if (result > 0) {
+            System.out.println("new raw is added");
+        }
+
         return data;
     }
 
 
+    @Override
+    public void run(String... args) throws Exception {
 
+    }
 }
