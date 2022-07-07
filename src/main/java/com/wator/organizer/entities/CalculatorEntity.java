3f02d23ca925be0c3ce014d989b03782e697b06a
utto2000 <bartek200039@gmail.com>
2022-07-08 01:07:16 +0200

    Fix Callculator entity and connection with database

--- src/main/java/com/wator/organizer/entities/CalculatorEntity.java
+++ src/main/java/com/wator/organizer/entities/CalculatorEntity.java
@@ -9,22 +9,32 @@ public class CalculatorEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "Id")
     private Integer id;
-
-    @Column(name = "userId")
+    @Column(name = "user_id")
     private Integer userId;
     @Column(name = "first_number")
     private Integer first_number;
-    @Column(name = "secondNumber")
+    @Column(name = "second_number")
     private Integer secondNumber;
+    @Column(name = "sign")
+    private char sign;
 
     public CalculatorEntity() {
     }
 
-    public CalculatorEntity(Integer id, Integer userId, Integer first_number, Integer secondNumber) {
+    public CalculatorEntity(Integer id, Integer userId, Integer first_number, Integer secondNumber,char sign) {
         this.id = id;
         this.userId = userId;
         this.first_number = first_number;
         this.secondNumber = secondNumber;
+        this.sign = sign;
+    }
+
+    public char getSign() {
+        return sign;
+    }
+
+    public void setSign(char sign) {
+        this.sign = sign;
     }
 
     public Integer getId() {
